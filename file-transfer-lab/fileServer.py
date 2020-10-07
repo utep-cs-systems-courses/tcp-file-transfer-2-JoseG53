9c1bf3e3f2125d59ebb1f59bd9f9850db9b21c86
Jose <jgallajr53@gmail.com>
2020-10-06 19:21:56 -0600

    Using FramedSock to send files

--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -2,21 +2,69 @@
 
 #Author: Jose Gallardo
 
-import socket, sys, re
+import socket, sys, re, os
+from framedSock import framedSend, framedReceive
 
 sys.path.append("../lib")
 import params
+os.chdir("Server")
 
 switchesVarDefaults = (
     (('l', '--listenPort'), 'listenPort', 50001),
     (('-?', '--usage'),"usage", False), #bool set if present
+    (('-d', '--debug'),"debug", False),
 )
 
 progname = "echoserver"
 paramMap = params.parseParams(switchesVarDefaults)
 
-listenPort = paramMap['listenPort']
-listenAddr = ''
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+#listenAddr = ''
 
 if paramMap['usage']:
     params.usage()
+
+listenerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+bindAddr = ("127.0.0.1", listenPort)
+listenerSocket.bind(bindAddr)
+listenerSocket.listen(5)
+while True:
+    print("listening on: ", bindAddr)
+
+    sock, addr = listenerSocket.accept()
+
+    rc = os.fork() #child to handle connections
+    if rc = 0:
+        print("Connection recieved from ", addr)
+
+       
+
+        start = framedReceive(sock, debug)
+        try:
+            start = start.decode()
+        except AttributeError:
+            print("error exiting: ", start)
+            sys.exit(0)
+
+        #where the file name ends
+        start = start.split("\'start\'")
+
+        #opening file
+        file = open(start[0].strip(), "wb+")
+
+        #recieving input while file has not ended
+        while True:
+            #error handling
+            try:
+                payload = framedReceive(sock, debug)
+
+            except:
+                pass
+
+            if debug: print("received: ", payload)
+            if not payload:
+                break
+
+        #ensures child exits loop        
+        break
+
